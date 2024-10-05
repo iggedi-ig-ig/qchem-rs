a4ba4f4a36495e8fd2a619d51715826262cf85ce
igs <x7041@protonmail.com>
2024-10-05 14:23:54 +0200

    test suite for molint

--- qchem-cli/src/main.rs
+++ qchem-cli/src/main.rs
@@ -3,6 +3,7 @@ use core::hf::{
 };
 use std::{error::Error, path::PathBuf};
 
+use approx::{assert_abs_diff_eq, assert_relative_eq, relative_eq};
 use clap::{ArgAction, Parser, Subcommand};
 use molint::{
     basis::BasisSet,
@@ -44,60 +45,64 @@ enum QcCommand {
 fn main() -> Result<(), Box<dyn Error>> {
     pretty_env_logger::init();
 
-    let args: Args = Args::parse();
+    let basis_set: BasisSet = BasisSet::load("data/basis/6-31G.json")?;
+    let molecule = MolecularSystem::from_atoms(
+        &[
+            Atom {
+                ordinal: 1,
+                position: Point3::new(0.4175, 0.0, 0.83),
+            },
+            Atom {
+                ordinal: 8,
+                position: Point3::new(0.0, 0.0, -0.31),
+            },
+            Atom {
+                ordinal: 1,
+                position: Point3::new(-0.4175, 0.0, 0.83),
+            },
+        ],
+        &basis_set,
+    );
 
-    match args.command {
-        QcCommand::RestrictedHartreeFock {
-            basis_set,
-            molecule,
-            max_iterations,
-            epsilon,
-        } => {
-            let basis_set: BasisSet = BasisSet::load(basis_set)?;
-            let molecule = MolecularSystem::from_atoms(
-                &[
-                    Atom {
-                        ordinal: 1,
-                        position: Point3::new(0.4175, 0.0, 0.83),
-                    },
-                    Atom {
-                        ordinal: 8,
-                        position: Point3::new(0.0, 0.0, -0.31),
-                    },
-                    Atom {
-                        ordinal: 1,
-                        position: Point3::new(-0.4175, 0.0, 0.83),
-                    },
-                ],
-                &basis_set,
+    let hf_output = restricted_hartree_fock(&HartreeFockInput {
+        system: molecule,
+        configuration: MolecularElectronConfig::ClosedShell,
+        max_iterations: 100,
+        epsilon: 1e-6,
+    });
+
+    match hf_output {
+        Some(RestrictedHartreeFockOutput {
+            ref orbital_energies,
+            electronic_energy,
+            ..
+        }) => {
+            assert_abs_diff_eq!(electronic_energy, -89.56219148143138, epsilon = 1e-6);
+
+            const ORBITAL_ENERGIES: [f64; 13] = [
+                -20.530046784284785,
+                -1.858642963156469,
+                -0.7310062353124118,
+                -0.711834352868239,
+                -0.5419965534828293,
+                0.20299276202600472,
+                0.36032431407863025,
+                1.028505309073029,
+                1.148278285162504,
+                1.1825266842623154,
+                1.5183562374176907,
+                1.668128908540347,
+                1.9522623427206827,
+            ];
+            assert!(
+                orbital_energies
+                    .iter()
+                    .enumerate()
+                    .all(|(i, &f)| relative_eq!(f, ORBITAL_ENERGIES[i], epsilon = 1e-6)),
+                "orbital energies didn't stay the same'"
             );
-
-            let hf_output = restricted_hartree_fock(&HartreeFockInput {
-                system: molecule,
-                configuration: MolecularElectronConfig::ClosedShell,
-                max_iterations,
-                epsilon,
-            });
-
-            match hf_output {
-                Some(
-                    ref output @ RestrictedHartreeFockOutput {
-                        ref orbital_energies,
-                        electronic_energy,
-                        nuclear_repulsion,
-                        iterations,
-                        ..
-                    },
-                ) => {
-                    println!("hartree fock converged after {iterations} iterations");
-                    println!("electronic energy: {electronic_energy:3.3}");
-                    println!("nuclear repulsion energy: {nuclear_repulsion:3.3}");
-                    println!("hartree fock energy: {:3.3}", output.total_energy());
-                    println!("orbital energies: {orbital_energies:3.3?}");
-                }
-                None => panic!("hartree fock did not converge"),
-            }
         }
+        None => panic!("hartree fock did not converge"),
     }
 
     Ok(())
